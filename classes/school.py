78d838b4862df300f5ec4e20beba050dc003a3de
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-05 18:25:34 -0400

    some work

--- classes/school.py
+++ classes/school.py
@@ -6,3 +6,13 @@ class School:
         self.name = name
         self.staff = Staff.objects()
         self.students = Student.objects()
+
+    def find_student_by_id(self, id):
+
+        check = next((item for item in self.students if item['school_id'] == id), 'there is no student with that id')
+        return check
+
+        
+    def list_students(self):
+        for item in self.students:
+            print(f"{item['name']} {item['school_id']}")
--- classes/student.py
+++ classes/student.py
@@ -4,18 +4,49 @@ from classes.person import Person
 
 class Student(Person):
 
-    def __init__(self, name, age, password, role, school_id):
-        super().__init__(name, age, password, role)
+    # def __init__(self, name, age, password, role, school_id):
+    #     super().__init__(name, age, password, role)
+    #     self.school_id = school_id
+
+    # @classmethod
+    # def objects(cls):
+    #     students = []
+    #     my_path = os.path.abspath(os.path.dirname(__file__))
+    #     path = os.path.join(my_path, "../data/students.csv")
+    #     with open(path) as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             students.append(Student(**dict(row)))
+    #     return students
+
+    student_list = []
+
+    def __init__(self, name, age, role, school_id, password):
+        super().__init__(name, age, role, password)
         self.school_id = school_id
 
     @classmethod
     def objects(cls):
-        students = []
-        my_path = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(my_path, "../data/students.csv")
-        with open(path) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                students.append(Student(**dict(row)))
-        return students
+        file_name = './data/students.csv'
+        
 
+        with open(file_name, newline='') as student_file:
+            reader = csv.DictReader(student_file)
+
+            for row in reader:
+                cls.student_list.append(row)
+
+        return cls.student_list
+
+    @classmethod
+    def add_student(cls, name, age, role, school_id, password):
+        cls.student_list.append({'name': name, 'age': age, 'role': role, 'school_id': school_id, 'password': password})
+
+    @classmethod
+    def remove_student(cls, school_id):
+        for i,item in enumerate(Student.student_list):
+            if school_id == item['school_id']:
+                Student.student_list.pop(i)
+
+    def __str__(self):
+        return (f'{self.name}\n********\nage: {self.age}\nid: {self.school_id}')
